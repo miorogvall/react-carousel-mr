2748145baec6c209cf8741dba30e7dcc5c9258e3
Mio Rogvall <mio.rogvall@gmail.com>
2020-08-07 22:23:00 +0200

    className fix

--- src/components/itemContainer/Item/Item.js
+++ src/components/itemContainer/Item/Item.js
@@ -13,10 +13,10 @@ class Item extends React.Component {
   return <div className={classList}>
     <img src={this.props.filename} alt="stort hus"></img>
     <div className="texts">
-      <div class="name">{this.props.name}</div>
-      <div class="desc">{this.props.description}</div>
-      <div class="rooms"><span className="bold">Antal rum: </span>{this.props.rooms} st</div>
-      <div class="bathrooms"><span className="bold">Antal badrum: </span>{this.props.bathrooms} st</div>
+      <div className="name">{this.props.name}</div>
+      <div className="desc">{this.props.description}</div>
+      <div className="rooms"><span className="bold">Antal rum: </span>{this.props.rooms} st</div>
+      <div className="bathrooms"><span className="bold">Antal badrum: </span>{this.props.bathrooms} st</div>
     </div>
   </div>
   }
