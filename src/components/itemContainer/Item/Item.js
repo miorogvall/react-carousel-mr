39642a59aa3cca89ff533ddef32326369ffe7f65
Mio Rogvall <mio.rogvall@gmail.com>
2020-08-07 22:21:53 +0200

    code cleanup

--- src/components/itemContainer/Item/Item.js
+++ src/components/itemContainer/Item/Item.js
@@ -5,21 +5,20 @@ class Item extends React.Component {
 
   render() {
     let classList;
-  if(this.props.activeNr === this.props.activeSlide) {
-     classList = 'item active'
-  } else {
-      classList = 'item'
-  }
-return <div className={classList}>
-  <img src={this.props.filename} alt="stort hus"></img>
-  <div className="texts">
-    <div class="name">{this.props.name}</div>
-    <div class="desc">{this.props.description}</div>
-    <div class="rooms"><span className="bold">Antal rum: </span>{this.props.rooms} st</div>
-    <div class="bathrooms"><span className="bold">Antal badrum: </span>{this.props.bathrooms} st</div>
+    if(this.props.activeNr === this.props.activeSlide) {
+      classList = 'item active'
+    } else {
+        classList = 'item'
+    }
+  return <div className={classList}>
+    <img src={this.props.filename} alt="stort hus"></img>
+    <div className="texts">
+      <div class="name">{this.props.name}</div>
+      <div class="desc">{this.props.description}</div>
+      <div class="rooms"><span className="bold">Antal rum: </span>{this.props.rooms} st</div>
+      <div class="bathrooms"><span className="bold">Antal badrum: </span>{this.props.bathrooms} st</div>
+    </div>
   </div>
-</div>
-
   }
 }
 
--- src/components/itemContainer/ItemContainer.js
+++ src/components/itemContainer/ItemContainer.js
@@ -5,24 +5,23 @@ class ItemContainer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-          count: 1,
-          leftInteractable: false,
-          rightInteractable: true
-        }
+            count: 1,
+            leftInteractable: false,
+            rightInteractable: true
     }
+}
 
     handleClick = (event) => {
         let currentTransform = event.target.parentNode.childNodes[0].style.transform.replace(/[^0-9\-]/g,'');
-            if(event.target.classList.contains('right')) {
-                if(this.state.count !== event.target.parentNode.childNodes[0].childNodes.length) {
-                    this.setState({
-                        count : this.state.count + 1
-                    })
-                    event.target.parentNode.childNodes[0].style = `transform: translate(${parseInt(currentTransform) - 90}%)`
-                } else {
-                }
-            } else {
-                if(this.state.count > 1) {
+        if(event.target.classList.contains('right')) {
+            if(this.state.count !== event.target.parentNode.childNodes[0].childNodes.length) {
+                this.setState({
+                    count : this.state.count + 1
+                })
+                event.target.parentNode.childNodes[0].style = `transform: translate(${parseInt(currentTransform) - 90}%)`
+            }
+        } else {
+            if(this.state.count > 1) {
                 event.target.parentNode.childNodes[0].style = `transform: translate(${parseInt(currentTransform) + 90}%)`
                 this.setState({
                     count : this.state.count - 1
@@ -34,24 +33,23 @@ class ItemContainer extends React.Component {
     render() {
         return <div className="item-container">
         <div className="items" style={{transform: 'translate(0%)'}}>
-            {this.props.children.map((data) => {
-                return <Item
-                    filename={data.filename}
-                    description={data.description}
-                    dimensions={data.dimensions}
-                    name={data.name}
-                    bathrooms={data.bathrooms}
-                    rooms={data.rooms}
-                    key={data.id}
-                    activeNr={data.activeNr}
-                    activeSlide={this.state.count}/>
-                })
-            }
+        {this.props.children.map((data) => {
+            return <Item
+                filename={data.filename}
+                description={data.description}
+                dimensions={data.dimensions}
+                name={data.name}
+                bathrooms={data.bathrooms}
+                rooms={data.rooms}
+                key={data.id}
+                activeNr={data.activeNr}
+                activeSlide={this.state.count}/>
+            })
+        }
         </div>
           <div className='fas fa-arrow-left arrow left' onClick={this.handleClick}></div>
           <div className='fas fa-arrow-right arrow right' onClick={this.handleClick}></div>
-
-      </div>
+        </div>
     }
 }
 export default ItemContainer;
