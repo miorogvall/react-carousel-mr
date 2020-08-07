edf71f388d1718be60d9c1495ed50e65a7437fd9
Mio Rogvall <mio.rogvall@gmail.com>
2020-08-07 13:25:44 +0200

    init + folder structure, sass install with save

---
+++ src/components/itemContainer/Item/Item.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Item.scss';
+
+function Item() {
+  return (
+      <div className="item"></div>
+  );
+}
+
+export default Item;
---
+++ src/components/itemContainer/ItemContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './_itemContainer.scss';
+
+function ItemContainer() {
+  return (
+      <div className="item-container">
+          <div>items here</div>
+      </div>
+  );
+}
+
+export default ItemContainer;
